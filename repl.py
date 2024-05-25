271252a3c8e38e77f879270a112f73d7c9e2c614
agamurian <agamurian@gmail.com>
2024-05-25 05:15:12 +0200

    some updates

--- repl.py
+++ repl.py
@@ -1,52 +1,61 @@
 # > catched changes
-import os
-import subprocess
-from typing import Callable
 import utils
-import shutil
 
-root = "/home/agamurian/gits"
+root = "C:\\Users\\anastasy\\gits"
+
 
 def process(path):
     print(path)
 
+
 def main():
     main_repl()
 
+
 def ls(*args):
-    utils.do_in_dir(root,utils.display_tree,*args)
+    utils.do_in_dir(root, utils.display_tree, *args)
     for arg in args:
-        utils.do_in_dir(arg,utils.display_tree,*args)
+        utils.do_in_dir(arg, utils.display_tree, *args)
+
 
 def dirs(*args):
-    utils.do_in_dir(root,utils.display_only_folders,*args)
+    utils.do_in_dir(root, utils.display_only_folders, *args)
     for arg in args:
-        utils.do_in_dir(arg,utils.display_only_folders,*args)
+        utils.do_in_dir(arg, utils.display_only_folders, *args)
+
 
 def pwd(*args):
     print(root)
 
+
+
+tree = lambda *args: utils.tree_print(root, *args)
 ext = lambda *args: utils.do_in_dir(root, utils.find_by_ext, *args)
 name = lambda *args: utils.do_in_dir(root, utils.find_by_name, *args)
 contains = lambda *args: utils.do_in_dir(root, utils.find_by_contains, *args)
 ends = lambda *args: utils.do_in_dir(root, utils.find_by_ends, *args)
 begins = lambda *args: utils.do_in_dir(root, utils.find_by_begins, *args)
 
+
 def cd(*args):
     global root
     for arg in args:
         root = arg
 
+
 dunder = " > "
+
+
 def main_repl():
-    print(f'{utils.sep}{utils.sep}')
-    print(dunder, end='')
+    print(f"{utils.sep}{utils.sep}")
+    print(dunder, end="")
     cmd = input()
-    ast = cmd.split(' ')
+    ast = cmd.split(" ")
     args = ast[1:]
     fun = lambda *args: eval(ast[0])
     function = fun(*args)
     function(*args)
     main_repl()
+
 
 main()
--- utils.py
+++ utils.py
@@ -2,79 +2,109 @@ import os
 import functools
 from threading import Timer
 
-sep="------------------------------------------------------"
+sep = "------------------------------------------------------"
+
+root = "C:\\Users\\anastasy\\gits"
+
+
+def tree_print(path, *args):
+    global previous_path
+    previous_path = root
+
+    def do_in_dir(path, func, level, *args):
+        for root, dirs, files in os.walk(path):
+            level = len(dirs)
+            for fullname in files:
+                path = os.path.join(root, *dirs, fullname)
+                name = path.replace(previous_path)
+                previous_path = path
+                func(name, level)
+
+    def func(name, level):
+        print(level)
+        print(name)
+
+    do_in_dir(path, func, level, *args)
+
 
 def find_by_ext(path, *args):
     fullname = os.path.split(path)[-1]
-    if len(fullname.split("."))==2:
-        name,ext = fullname.split(".")
+    if len(fullname.split(".")) == 2:
+        name, ext = fullname.split(".")
         for arg in args:
             if ext == arg:
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
-    if len(fullname.split("."))==1:
+    if len(fullname.split(".")) == 1:
         name = fullname
+
 
 def find_by_name(path, *args):
     fullname = os.path.split(path)[-1]
-    if len(fullname.split("."))==2:
-        name,ext = fullname.split(".")
+    if len(fullname.split(".")) == 2:
+        name, ext = fullname.split(".")
         for arg in args:
             if name == arg:
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
-    if len(fullname.split("."))==1:
+    if len(fullname.split(".")) == 1:
         name = fullname
         for arg in args:
             if name == arg:
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
+
 
 def find_by_contains(path, *args):
     fullname = os.path.split(path)[-1]
-    if len(fullname.split("."))==2:
-        name,ext = fullname.split(".")
+    if len(fullname.split(".")) == 2:
+        name, ext = fullname.split(".")
         for arg in args:
             if arg in name:
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
-    if len(fullname.split("."))==1:
+    if len(fullname.split(".")) == 1:
         name = fullname
+
 
 def find_by_ends(path, *args):
     fullname = os.path.split(path)[-1]
-    if len(fullname.split("."))==2:
-        name,ext = fullname.split(".")
+    if len(fullname.split(".")) == 2:
+        name, ext = fullname.split(".")
         for arg in args:
             if name.endswith(arg):
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
-    if len(fullname.split("."))==1:
+    if len(fullname.split(".")) == 1:
         name = fullname
+
 
 def find_by_begins(path, *args):
     fullname = os.path.split(path)[-1]
-    if len(fullname.split("."))==2:
-        name,ext = fullname.split(".")
+    if len(fullname.split(".")) == 2:
+        name, ext = fullname.split(".")
         for arg in args:
             if name.endswith(arg):
                 print(sep)
-                print(path.replace(fullname,''))
+                print(path.replace(fullname, ""))
                 print(fullname)
-    if len(fullname.split("."))==1:
+    if len(fullname.split(".")) == 1:
         name = fullname
+
 
 def no_doubles(lst):
     return list(set(lst))
 
+
 def do_in_list(lst, func, *args):
     for path in lst:
         func(path, *args)
+
 
 def do_in_dir(path, func, *args):
     for root, dirs, files in os.walk(path):
@@ -82,18 +112,23 @@ def do_in_dir(path, func, *args):
             path = os.path.join(root, *dirs, fullname)
             func(path, *args)
 
-ignore = ['node_modules','.npm']
+
+ignore = ["node_modules", ".npm", ".git"]
+
+
 def filter(path):
     for word in ignore:
         if word in path:
             return False
     return True
 
-def display_tree(path,*args):
+
+def display_tree(path, *args):
     if filter(path):
         print(path)
 
-def display_only_folders(path,*args):
+
+def display_only_folders(path, *args):
     array = os.path.split(path)
     for a in array:
         a.pop
@@ -104,6 +139,7 @@ def display_only_folders(path,*args):
     if filter(path):
         print(path)
 
+
 def debounce(timeout: float):
     def decorator(func):
         @functools.wraps(func)
@@ -111,6 +147,8 @@ def debounce(timeout: float):
             wrapper.func.cancel()
             wrapper.func = Timer(timeout, func, args, kwargs)
             wrapper.func.start()
+
         wrapper.func = Timer(timeout, lambda: None)
         return wrapper
+
     return decorator
